c6516f076be9c92fad4387831726be028cf9450b
Markus Lamm <markus.lamm@googlemail.com>
2013-05-20 20:11:18 +0200

    working head position calc

--- models/robot.js
+++ models/robot.js
@@ -10,7 +10,7 @@ define([ "util", "vbo", "models/cube", "models/band", "models/triangle", "scene_
 
 		this.programs = programs;
 
-		// create some objects to be used for drawing the roboter
+		/* create objects for drawing the roboter */
 		var triangle = new Triangle(gl);
 		var cube = new Cube(gl);
 		var band = new Band(gl,  { radius: 0.5, height: 1.0, segments: 50 });
@@ -18,44 +18,54 @@ define([ "util", "vbo", "models/cube", "models/band", "models/triangle", "scene_
 		});
 
 		/* dimensions */
-		var torsoSize = [ 0.6, 1.0, 0.4 ];
-		var jointSize = [ 0.2, 0.1, 0.2 ];
-		var headSize = [ 0.3, 0.4, 0.3 ];
+		var torsoSize 	= [0.6, 1.0, 0.4];
+		var jointSize 	= [0.2, 0.1, 0.2];
+		var headSize 	= [0.3, 0.4, 0.3];
+		
+		var upperArmSize = [0.15, 0.45, 0.15];
+        var lowerArmSize = [0.15, 0.45, 0.15];
 
-		var torsoPosition = [ 0.0, 0, 0.0 ];
-		// y = torsoSize.y/2 + jointSize.y/2 = 0.55
-		var neckPosition = [ 0.0, 0.55, 0.0 ];
+		/* positions */
+		var torsoPosition	= [0.0, 0, 0.0];
+		var neckPosition	= [0.0, torsoSize[1]/2 + jointSize[1]/2, 0.0];
+		console.log("neckPostion: " + neckPosition);
+		var headPosition	= [0.0, jointSize[1]/2 + headSize[1]/2, 0.0]; 
+		console.log("headPostion: " + headPosition);
 
-		// y = joint.dim.y 0.1/2 + head.dim.y +0.4/2
-		var headPosition = [ 0.0, 0.25, 0.0 ];
-
+		/* creating skeleton objects and set position in scene */
 		var torso = new SceneNode("torso");
-
-		var neck = new SceneNode("neck");
-
-		// torso skin
-		var torsoSkin = new SceneNode("torso skin", [ cube ], this.programs.vertexColor);
-		mat4.scale(torsoSkin.transformation, torsoSize);
-		mat4.rotate(torsoSkin.transformation, 1 * Math.PI / 2, [ 0, 1, 0 ]); // blue
-		// front,
-		// red
-		// top,
-		// green
-		// sides
-
-		var joinSkin = new SceneNode("joint skin", [ band ], this.programs.red);
-		mat4.scale(joinSkin.transformation, jointSize);
-
-		mat4.translate(neck.transformation, neckPosition);
 		mat4.translate(torso.transformation, torsoPosition);
 
-		torso.addObjects([ torsoSkin ]);
-		neck.addObjects([ joinSkin ]);
+		var neck = new SceneNode("neck");
+		mat4.translate(neck.transformation, neckPosition);
+		
+		var head = new SceneNode("head");
+		mat4.translate(head.transformation, headPosition);
+		
+		
+		
+		/*creating skins */
+		var torsoSkin = new SceneNode("torso skin", [cube], this.programs.vertexColor);
+		mat4.scale(torsoSkin.transformation, torsoSize);
+		mat4.rotate(torsoSkin.transformation, 1 * Math.PI / 2, [0, 1, 0]); // blue in front, just optional
 
-		torso.addObjects([ neck ]);
+		var joinSkin = new SceneNode("joint skin", [band], this.programs.red);
+		mat4.scale(joinSkin.transformation, jointSize);
+		
+		var headSkin = new SceneNode("head skin", [cube], this.programs.vertexColor);
+		mat4.scale(headSkin.transformation, headSize);
 
-		// the final robot
-		this.result = new SceneNode("robot", [ torso ], this.programs.red);
+		/* connect skeleton with skins */
+		torso.addObjects([torsoSkin]);
+		neck.addObjects([joinSkin]);
+		head.addObjects([headSkin]);
+		
+		/* creating scenegraph */
+		neck.addObjects([head]);
+		torso.addObjects([neck]);
+
+		/* the final robot */
+		this.result = new SceneNode("robot", [torso], this.programs.red);
 		mat4.translate(this.result.transformation, [ 0, 0, 0 ]);
 	};
 
